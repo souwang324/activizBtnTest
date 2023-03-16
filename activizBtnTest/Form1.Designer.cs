cec120211c73c7c9e80c4f8420786b636f9f5e53
souwang324 <33944965+souwang324@users.noreply.github.com>
2023-03-16 22:08:57 +0900

    Initial commit

---
+++ activizBtnTest/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace activizBtnTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtnPoints = new System.Windows.Forms.RadioButton();
+            this.radioBtnWire = new System.Windows.Forms.RadioButton();
+            this.radioBtnSurf = new System.Windows.Forms.RadioButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1220, 673);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtnPoints);
+            this.groupBox1.Controls.Add(this.radioBtnWire);
+            this.groupBox1.Controls.Add(this.radioBtnSurf);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(1229, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(172, 673);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Property";
+            // 
+            // radioBtnPoints
+            // 
+            this.radioBtnPoints.AutoSize = true;
+            this.radioBtnPoints.Location = new System.Drawing.Point(28, 265);
+            this.radioBtnPoints.Name = "radioBtnPoints";
+            this.radioBtnPoints.Size = new System.Drawing.Size(70, 19);
+            this.radioBtnPoints.TabIndex = 0;
+            this.radioBtnPoints.TabStop = true;
+            this.radioBtnPoints.Text = "Points";
+            this.radioBtnPoints.UseVisualStyleBackColor = true;
+            this.radioBtnPoints.CheckedChanged += new System.EventHandler(this.radioBtnPoints_CheckedChanged);
+            // 
+            // radioBtnWire
+            // 
+            this.radioBtnWire.AutoSize = true;
+            this.radioBtnWire.Location = new System.Drawing.Point(28, 178);
+            this.radioBtnWire.Name = "radioBtnWire";
+            this.radioBtnWire.Size = new System.Drawing.Size(92, 19);
+            this.radioBtnWire.TabIndex = 0;
+            this.radioBtnWire.TabStop = true;
+            this.radioBtnWire.Text = "Wireframe";
+            this.radioBtnWire.UseVisualStyleBackColor = true;
+            this.radioBtnWire.CheckedChanged += new System.EventHandler(this.radioBtnWire_CheckedChanged);
+            // 
+            // radioBtnSurf
+            // 
+            this.radioBtnSurf.AutoSize = true;
+            this.radioBtnSurf.Checked = true;
+            this.radioBtnSurf.Location = new System.Drawing.Point(28, 92);
+            this.radioBtnSurf.Name = "radioBtnSurf";
+            this.radioBtnSurf.Size = new System.Drawing.Size(78, 19);
+            this.radioBtnSurf.TabIndex = 0;
+            this.radioBtnSurf.TabStop = true;
+            this.radioBtnSurf.Text = "Surface";
+            this.radioBtnSurf.UseVisualStyleBackColor = true;
+            this.radioBtnSurf.CheckedChanged += new System.EventHandler(this.radioBtnSurf_CheckedChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.32194F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.67806F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1404, 679);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 679);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtnPoints;
+        private System.Windows.Forms.RadioButton radioBtnWire;
+        private System.Windows.Forms.RadioButton radioBtnSurf;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ activizBtnTest/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Kitware.VTK;
+
+namespace activizBtnTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.renderWindowControl = new Kitware.VTK.RenderWindowControl();
+            this.renderWindowControl.AddTestActors = false;
+            this.renderWindowControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.renderWindowControl.Location = new System.Drawing.Point(0, 0);
+            this.renderWindowControl.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.renderWindowControl.Name = "renderWindowControl";
+            this.renderWindowControl.Size = new System.Drawing.Size(617, 347);
+            this.renderWindowControl.TabIndex = 0;
+            this.renderWindowControl.TestText = null;
+            this.renderWindowControl.Load += new System.EventHandler(this.renderWindowControl_Load);
+            pictureBox1.Controls.Add(this.renderWindowControl);
+        }
+
+        private void renderWindowControl_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ReadSTL();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK);
+            }
+        }
+        public Kitware.VTK.RenderWindowControl renderWindowControl;
+
+        vtkPolyDataMapper mapper;
+        vtkActor[] actors;
+        vtkRenderWindow renderWindow;
+        vtkRenderer renderer;
+
+        private void ReadSTL()
+        {
+            // Path to vtk data must be set as an environment variable
+            // VTK_DATA_ROOT = "C:\VTK\vtkdata-5.8.0"
+            string root = System.IO.Directory.GetCurrentDirectory(); // test.GetDataRoot();
+            Trace.WriteLine(root);
+            //string filePath = System.IO.Path.Combine(root, @"..\..\res\42400-IDGH.stl");
+            string filePath = System.IO.Path.Combine(root, @"..\..\res\shark.ply");
+            //string filePath = System.IO.Path.Combine(root, @"..\..\res\Torso.vtp");
+            vtkPolyData polyData = ReadPolyData(filePath);
+            mapper = vtkPolyDataMapper.New();
+            mapper.SetInput(polyData);
+
+           actors = new vtkActor[3];
+            actors[0] = vtkActor.New();
+            actors[0].SetMapper(mapper);
+            actors[0].GetProperty().SetRepresentationToSurface();
+
+           actors[1] = vtkActor.New();
+            actors[1].SetMapper(mapper);
+            actors[1].GetProperty().SetRepresentationToWireframe();
+
+           actors[2] = vtkActor.New();
+            actors[2].SetMapper(mapper);
+            actors[2].GetProperty().SetRepresentationToPoints();
+
+           // get a reference to the renderwindow of our renderWindowControl1
+           renderWindow = renderWindowControl.RenderWindow;
+            // renderer
+            renderer = renderWindow.GetRenderers().GetFirstRenderer();
+            // set background color
+            renderer.SetBackground(0.2, 0.3, 0.4);
+            // add our actor to the renderer
+            renderer.AddActor(actors[0]);
+            refresh();
+        }
+
+        vtkPolyData ReadPolyData(string fileName)
+        {
+            vtkPolyData polyData = vtkPolyData.New();
+            string extension = System.IO.Path.GetExtension(fileName);
+            if (extension == ".ply")
+            {
+                vtkPLYReader reader = vtkPLYReader.New();
+                reader.SetFileName(fileName);
+                reader.Update();
+                polyData = reader.GetOutput();
+            }
+            else if (extension == ".vtp")
+            {
+                vtkXMLPolyDataReader reader = vtkXMLPolyDataReader.New();
+                reader.SetFileName(fileName);
+                reader.Update();
+                polyData = reader.GetOutput();
+            }
+            else if (extension == ".obj")
+            {
+                vtkOBJReader reader = vtkOBJReader.New();
+                reader.SetFileName(fileName);
+                reader.Update();
+                polyData = reader.GetOutput();
+            }
+            else if (extension == ".stl")
+            {
+                vtkSTLReader reader = vtkSTLReader.New();
+                reader.SetFileName(fileName);
+                reader.Update();
+                polyData = reader.GetOutput();
+            }
+            else if (extension == ".vtk")
+            {
+                vtkPolyDataReader reader = vtkPolyDataReader.New();
+                reader.SetFileName(fileName);
+                reader.Update();
+                polyData = reader.GetOutput();
+            }
+            else if (extension == ".g")
+            {
+                vtkBYUReader reader = vtkBYUReader.New();
+                reader.SetGeometryFileName(fileName);
+                reader.Update();
+                polyData = reader.GetOutput();
+            }
+            else
+            {
+                vtkSphereSource source = vtkSphereSource.New();
+                source.Update();
+                polyData = source.GetOutput();
+            }
+            return polyData;
+        }
+
+        private void refresh()
+        {
+            renderWindow.AddRenderer(renderer);
+            renderWindow.Render();
+        }
+
+        private void radioBtnSurf_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBtnSurf.Checked)
+            {
+                renderer.RemoveAllViewProps();
+                renderer.AddActor(actors[0]);
+                Trace.WriteLine("surface");
+                refresh();
+            }
+        }
+
+        private void radioBtnWire_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBtnWire.Checked)
+            {
+                renderer.RemoveAllViewProps();
+                renderer.AddActor(actors[1]);
+                Trace.WriteLine("wirefame");
+                refresh();
+            }
+        }
+
+        private void radioBtnPoints_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBtnPoints.Checked)
+            {
+                renderer.RemoveAllViewProps();
+                renderer.AddActor(actors[2]);
+                Trace.WriteLine("points");
+                refresh();
+            }
+        }
+    }
+}
